1cb27d8ebe46af76deaf8080c1f65cb647137109
meorm morits <149721455+meorm-mortis@users.noreply.github.com>
2024-11-16 10:41:16 -0700

    Add files via upload

---
+++ un-fuck-up.bat
@@ -0,0 +1,86 @@
+@echo off
+
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+	echo Please right click and select run as administrator.
+	pause
+	exit
+    )
+
+    pause
+
+echo Stopping Background Services...
+net stop bits
+net stop wuauserv
+echo y|net stop appidsvc
+echo y|net stop cryptsvc
+
+echo y|Del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\*.*"
+
+echo y|rmdir %systemroot%\SoftwareDistribution /S /Q
+echo y|rmdir %systemroot%\system32\catroot2 /S /Q
+
+echo Reseting Windows Update Services...
+sc.exe sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+sc.exe sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+
+cd /d %windir%\system32
+
+regsvr32.exe /s atl.dll
+regsvr32.exe /s urlmon.dll
+regsvr32.exe /s mshtml.dll
+regsvr32.exe /s shdocvw.dll
+regsvr32.exe /s browseui.dll
+regsvr32.exe /s jscript.dll
+regsvr32.exe /s vbscript.dll
+regsvr32.exe /s scrrun.dll
+regsvr32.exe /s msxml.dll
+regsvr32.exe /s msxml3.dll
+regsvr32.exe /s msxml6.dll
+regsvr32.exe /s actxprxy.dll
+regsvr32.exe /s softpub.dll
+regsvr32.exe /s wintrust.dll
+regsvr32.exe /s dssenh.dll
+regsvr32.exe /s rsaenh.dll
+regsvr32.exe /s gpkcsp.dll
+regsvr32.exe /s sccbase.dll
+regsvr32.exe /s slbcsp.dll
+regsvr32.exe /s cryptdlg.dll
+regsvr32.exe /s oleaut32.dll
+regsvr32.exe /s ole32.dll
+regsvr32.exe /s shell32.dll
+regsvr32.exe /s initpki.dll
+regsvr32.exe /s wuapi.dll
+regsvr32.exe /s wuaueng.dll
+regsvr32.exe /s wuaueng1.dll
+regsvr32.exe /s wucltui.dll
+regsvr32.exe /s wups.dll
+regsvr32.exe /s wups2.dll
+regsvr32.exe /s wuweb.dll
+regsvr32.exe /s qmgr.dll
+regsvr32.exe /s qmgrprxy.dll
+regsvr32.exe /s wucltux.dll
+regsvr32.exe /s muweb.dll
+regsvr32.exe /s wuwebv.dll
+
+netsh winsock reset
+netsh winsock reset proxy
+
+echo Starting Background Services
+net start bits
+net start wuauserv
+net start appidsvc
+net start cryptsvc
+
+
+echo Script Complete! 
+echo Please reboot your computer as soon as possible.
+pause
